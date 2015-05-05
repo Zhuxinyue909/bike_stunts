4949386fcc3c213c0958a18f3b4a3b0907ea00c0
Zhuxinyue909 <1090589429@qq.com>
2015-05-05 15:55:04 -0400

    eee

---
+++ ode-0.13/build/bike-ode/define.h
@@ -0,0 +1,26 @@
+#define SQRT3 1.732
+#define LENGTH 0.7
+#define bot_LEN 0.6062//0.6062  LENGTH*SQRT3/2.0
+	// chassis length
+#define WIDTH 0.03
+#define HEIGHT 0.03	// chassis height
+#define WHEEL_WIDTH 0.03
+#define RADIUS 0.18	// wheel radius
+#define STARTZ 0.18	// starting height of chassis
+#define CMASS 0.1		// chassis mass
+#define WMASS 2	// wheel mass
+#define BAR_LEN 0.45
+#define pi 3.1415
+#define time_step 0.04
+#define Num 16
+	#define LENGHTH_1 0.03 
+	#define LENGHTH_2 0.07 
+	#define LENGHTH_3 0.11 
+	#define LENGHTH_4 0.15 
+	#define LENGHTH_5 0.24 
+	#define LENGHTH_6 0.30 
+
+	#define RADIUS_LARGE 0.09 
+	#define RADIUS_MEDIUM 0.03 
+    #define RADIUS_MEDLARGE 0.06 
+	#define RADIUS_SMALL 0.02 
